244157e8497df2369b04ac4e6f358ea4c719858c
caiyonglong <caiyonglong@live.com>
2018-03-28 19:24:55 +0800

    完成任务三

--- README.md
+++ README.md
@@ -1,8 +1,16 @@
 # DrawApp
 HenCoder Android 开发进阶练习项目
+![HenCoder](https://github.com/hencoder)
 
-##已完成
+## 已完成
 
 HenCoder 绘制 1 练习项目
+![](images/preview1.png)
 
 HenCoder 绘制 2 练习项目
+
+![](images/preview2.png)
+
+HenCoder 绘制 3 练习项目
+
+![](images/preview3.png)
--- app/src/main/java/com/cyl/drawapp/DrawActivity.java
+++ app/src/main/java/com/cyl/drawapp/DrawActivity.java
@@ -4,10 +4,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 
 import com.cyl.drawapp.hencoderpracticedraw1.TestActivity1;
 import com.cyl.drawapp.hencoderpracticedraw2.TestActivity2;
+import com.cyl.drawapp.hencoderpracticedraw3.TestActivity3;
 
 /**
  * Created by D22434 on 2017/10/18.
@@ -15,30 +15,31 @@ import com.cyl.drawapp.hencoderpracticedraw2.TestActivity2;
 
 public class DrawActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Button mBtnDraw1, mBtnDraw2;
+    int[] viewIds = new int[]{R.id.btn_draw1, R.id.btn_draw2,
+            R.id.btn_draw3
+    };
+    Class[] TargetClasses = new Class[]{
+            TestActivity1.class,
+            TestActivity2.class,
+            TestActivity3.class,
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_draw);
-        mBtnDraw1 = findViewById(R.id.btn_draw1);
-        mBtnDraw2 = findViewById(R.id.btn_draw2);
-        mBtnDraw1.setOnClickListener(this);
-        mBtnDraw2.setOnClickListener(this);
+        for (int i = 0; i < viewIds.length; i++) {
+            findViewById(viewIds[i]).setOnClickListener(this);
+        }
     }
 
     @Override
     public void onClick(View v) {
-        Intent intent = null;
-        switch (v.getId()) {
-            case R.id.btn_draw1:
-                intent = new Intent(DrawActivity.this, TestActivity1.class);
-                startActivity(intent);
-                break;
-            case R.id.btn_draw2:
-                intent = new Intent(DrawActivity.this, TestActivity2.class);
-                startActivity(intent);
-                break;
+        for (int i = 0; i < viewIds.length; i++) {
+            if (v.getId() == viewIds[i]) {
+                startActivity(new Intent(DrawActivity.this,
+                        TargetClasses[i]));
+            }
         }
     }
 }
